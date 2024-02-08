0e4c75a5efcf42ad0d27a5c5e01fe0c3318426ea
KoutaKawase <suhrkawase@gmail.com>
2024-02-08 15:59:12 +0900

    feat: 🎸 wip

--- src/arkanoid.ts
+++ src/arkanoid.ts
@@ -8,10 +8,12 @@ const plate = {
 };
 
 let ball = {
-	diameter: 15,
+	radius: 7,
 	x: 0,
 	y: 0,
+	vx: 10,
 	vy: 10,
+	angle: 80,
 	isFlying: false,
 };
 
@@ -19,13 +21,15 @@ const screen = {
 	width: 600,
 	height: 600,
 };
-const fps = 30;
+
+const fps = 40;
 
 const sketch = (p: p5) => {
 	p.setup = () => {
 		p.createCanvas(screen.width, screen.height);
 		p.frameRate(fps);
-		ball.y = plate.y - plate.height + 5;
+		p.angleMode(p.DEGREES);
+		ball.y = plate.y - 5;
 	};
 
 	p.draw = () => {
@@ -34,16 +38,26 @@ const sketch = (p: p5) => {
 		plate.x = p.constrain(p.mouseX, 0, screen.width - plate.width);
 		p.rect(plate.x, plate.y, plate.width, plate.height);
 
-		ball.x = p.constrain(
-			p.mouseX + plate.width / 2,
-			0,
-			screen.width - plate.width / 2,
-		);
-		p.circle(ball.x, ball.y, ball.diameter);
-
 		if (ball.isFlying) {
-			ball.y -= ball.vy;
+			ball.x += p.cos(ball.angle) * ball.vx;
+			ball.y -= p.sin(ball.angle) * ball.vy;
+
+			if (ball.x + ball.radius > screen.width || ball.x - ball.radius < 0) {
+				ball.vx *= -1;
+			}
+			if (ball.y + ball.radius > screen.height || ball.y - ball.radius < 0) {
+				ball.vy *= -1;
+			}
+		} else {
+			ball.x = p.constrain(
+				p.mouseX + plate.width / 2,
+				0,
+				screen.width - plate.width / 2,
+			);
 		}
+
+		ball.y = p.constrain(ball.y, 0 + ball.radius, screen.height);
+		p.circle(ball.x, ball.y, ball.radius * 2);
 	};
 
 	p.mouseClicked = () => {
